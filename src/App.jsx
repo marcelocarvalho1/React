a12237576828417608cac7bbf12f2c6cae294e0f
Marcelo <marceloo.dev@gmail.com>
2025-05-27 20:44:23 -0300

    adicionando props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,40 @@
+import { useState } from "react";
 import { AddTask } from "./components/AddTask";
-import { Task } from "./components/Task";
+import { Tasks } from "./components/Tasks";
+import "./index.css";
 
 export const App = () => {
+  const [tasks, seTasks] = useState([
+    {
+      id: 1,
+      title: "estudar",
+      descripition: "Desenvolver",
+      isCompleted: false,
+    },
+      {
+      id: 2,
+      title: "estudar2",
+      descripition: "Desenvolver2",
+      isCompleted: false,
+    },
+      {
+      id: 3,
+      title: "estudar3",
+      descripition: "Desenvolver3",
+      isCompleted: false,
+    },
+  ]);
+
   return (
-    <div>
-      <h1> Gerenciador de Taferas</h1>
-      <AddTask />
-      <Task />
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          {" "}
+          Gerenciador de Taferas
+        </h1>
+        <AddTask />
+        <Tasks tasks={tasks} />
+      </div>
     </div>
   );
 };

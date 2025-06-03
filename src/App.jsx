ed848aed56870a4bfd09bc9b2f44f54c8ae8a530
Marcelo <marceloo.dev@gmail.com>
2025-06-02 21:10:17 -0300

    Adicionndoa funcionalidade de exclusão de tarefas

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Tasks } from "./components/Tasks";
 import "./index.css";
 
 export const App = () => {
-  const [tasks, seTasks] = useState([
+  const [tasks, setTasks] = useState([
     {
       id: 1,
       title: "Estudar programação",
@@ -27,24 +27,36 @@ export const App = () => {
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
+      //Preciso atualizar essa tarefa
       if (task.id === taskId) {
         return { ...task, isCompleted: !task.isCompleted };
       }
+      //Não preciso atualizar essa tarefa
       return task;
     });
 
-    seTasks(newTasks);
+    setTasks(newTasks);
+
   }
+
+    function onDeleteTaskClick(taskId) {
+      const newTasks = tasks.filter(task => task.id !== taskId);
+      setTasks(newTasks);
+    }
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px]">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           {" "}
-          Gerenciador de Taferas
+          Gerenciador de Tarefas
         </h1>
         <AddTask />
-        <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
     </div>
   );

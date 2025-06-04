a38505861ab083f01f914e33b276a840556fc0df
Marcelo <marceloo.dev@gmail.com>
2025-06-04 09:09:42 -0300

    Atualiza a navegação de detalhes da tarefa

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -1,4 +1,5 @@
 import { useSearchParams } from "react-router-dom";
+import {ChevronLeftIcon} from "lucide-react"
 
 export const TaskPage = () => {
   const [searchParams] = useSearchParams();
@@ -6,8 +7,25 @@ export const TaskPage = () => {
   const description = searchParams.get("description");
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+      <div className="w-[500px] space-y-4">
+        <div className="flex justify-center relative mb-6">
+
+      {/* botão voltar */}
+          <button className="absolute left-0" >
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Detalhes da tarefa
+          </h1>
+        </div>
+
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h1 className="text-xl text-white font-bold text-slate-600">
+            {title}
+          </h1>
+          <p className="text-slate-600">{description} </p>
+        </div>
+      </div>
     </div>
   );
 };
